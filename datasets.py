Fix the scaling for MAF, so that the log likelihoods are comparable to previous work.

datasets.py
@@ -63,9 +63,10 @@ def _get_mnist(data_dir, batch_size, split="train", binarized=None, repeat=True,
     # Add uniform dequantization jitter
     def jitter_im(im, alpha):
       jitter_noise = tfd.Uniform(low=tf.zeros_like(im), high=tf.ones_like(im)).sample()
-      jittered_im = (im * 255. + jitter_noise) / 256.
-      squashed_im = (jittered_im - 0.5) * (1 - alpha) + 0.5
-      return squashed_im
+      jittered_im = im * 255. + jitter_noise
+      return jittered_im
+      #squashed_im = (jittered_im - 128.) * (1 - alpha) + 128.
+      #return squashed_im
 
     dataset = dataset.map(lambda im: jitter_im(im, alpha=1e-6))
 

models/maf.py
@@ -16,6 +16,7 @@ class MAF(object):
                flow_layers,
                proposal=None,
                data_mean=None,
+               alpha=1e-6,
                dtype=tf.float32,
                name="maf"):
     """Creates a MAF model.
@@ -40,7 +41,9 @@ class MAF(object):
       self.proposal = proposal
 
 
-    bijectors = [tfb.Sigmoid()]
+    bijectors = [tfb.AffineScalar(scale=256.),
+                 tfb.AffineScalar(scale=1./(1. - alpha), shift=0.5*(1. - 1./(1.-alpha))),
+                 tfb.Sigmoid()]
     for _ in range(flow_layers):
       bijectors.append(tfb.MaskedAutoregressiveFlow(
           shift_and_log_scale_fn=tfb.masked_autoregressive_default_template(